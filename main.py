75160c3613cbfc5ee9c22d166fb8c1b87964c7e6
Lyreplus <lyreplus@gmail.com>
2022-06-09 15:05:23 +0200

    added comments, news scrolling fixed

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import feedparser
 import time
 from os import system
-NewsFeed = feedparser.parse("http://xml2.corriereobjects.it/rss/homepage.xml")
+NewsFeed = feedparser.parse("http://xml2.corriereobjects.it/rss/homepage.xml") #your newspaper's rss
 
 print("Number of  RSS post: ", len(NewsFeed.entries))
 
+topic = ["meteo"] #insert your topic(s)
+
 for n, entry in enumerate(NewsFeed.entries):
-  for j in ("Ukraina", "Russia", "Zelensky", "Draghi", "Guerra", "Putin"):
+  for j in (topic):
     if j.lower() in entry.title.lower():
       a = str(entry.title)
       b = len(a)
       c = 0
       d = 20
-      while d < b:
+      while d <= b:
           print(a[c:d])
           c += 1
           d += 1
@@ -22,5 +24,3 @@ for n, entry in enumerate(NewsFeed.entries):
 
 print("\n\n------------")
 
-
-print(New)
